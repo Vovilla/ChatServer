afd9d8f7bc3e0277fab9b37e32b2012af3c14e52
Vovilla <ciscovovus@gmail.com>
2022-06-01 20:50:52 +0300

    private_msg is ready

--- ChatServer/ChatServer.cpp
+++ ChatServer/ChatServer.cpp
@@ -5,8 +5,10 @@
 #include <regex>
 #include <cctype>
 #include <uwebsockets/App.h>
+#include <nlohmann/json.hpp>
 
 using namespace std;
+using json = nlohmann::json;
 
 string ToLower(string text) {
 	transform(text.begin(), text.end(),
@@ -39,16 +41,41 @@ struct UserData {
 	int user_id;
 };
 
+const string COMMAND = "command";
+const string PRIVATE_MSG = "PRIVATE_MSG";
+const string PUBLIC_MSG = "PUBLIC_MSG";
+const string USER_ID_TO = "user_id_to";
+const string USER_ID_FROM = "user_id_from";
+const string TEXT = "text";
+
+void ProcessMessage(auto ws, auto message) {
+	json data = json::parse(message);
+	UserData* userData = ws->getUserData();
+	string command = data[COMMAND];
+	if(command == PRIVATE_MSG) {
+		int user_id = data[USER_ID_TO];
+		string text = data[TEXT];
+		json response;
+		response[COMMAND] = PRIVATE_MSG;
+		response[USER_ID_FROM] = userData->user_id;
+		response[TEXT] = text;
+		ws->publish("user" + to_string(data->user_id), response.dump());
+		cout << "User " << userData->user_id 
+			<< " send message to User N " << user_id << endl;
+	}
+}
 
 int main()
 {
+	int latest_user_id = 1;
 	uWS::App().get("/hello", [](auto* res, auto* req) {
 		res->writeHeader("Content-Type", "text/html; charset=utf-8")->end("Hello HTTP!");
 		}).ws<UserData>("/*", {
-			.open = [](auto* ws) {
+			.open = [&latest_user_id](auto* ws) {
 				UserData* data = ws->getUserData();
-				//ws->getUserData
-				//ws->subscribe("sensors/+/house");
+				data->user_id = latest_user_id++;
+				cout << "User connected ID= " << data->user_id << endl;
+				ws->subscribe("user" + to_string(data->user_id));
 			},
 			.message = [](auto* ws, string_view message, uWS::OpCode opCode) {
 				//ws->send(message, opCode);
